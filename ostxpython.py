f79dcb4ecd0b196943531bd77ed4432d4ad6b3d2
mahesh25111 <113080140+mahesh25111@users.noreply.github.com>
2022-09-08 08:58:32 +0530

    Add files via upload

---
+++ ostxpython.py
@@ -0,0 +1,7 @@
+Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+"hello world"
+'hello world'
+
+===== RESTART: C:/Users/Lenovo/AppData/Local/Programs/Python/Python310/a.py ====
+hello world
